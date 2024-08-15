2f4f28202e4613bbdaca251c5a374a2299c40046
fabri <fabricioalama2001@outlook.es>
2024-08-14 23:47:14 -0500

    Add project files.

---
+++ ExamenVelasco/CAD/ArtistaDAL.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using ExamenVelasco.Modelos;
+
+namespace ExamenVelasco.CAD
+{
+    public class ArtistaDAL
+    {
+        // Añadir un nuevo artista a la base de datos
+        public bool AñadirArtista(Artista artista)
+        {
+            using (ConexionDB db = new ConexionDB())
+            {
+                string sql = "INSERT INTO Artistas (nombre, apellido, fecha_nacimiento, nacionalidad) VALUES (@nombre, @apellido, @fechaNacimiento, @nacionalidad)";
+                SqlCommand cmd = new SqlCommand(sql, db.Conexion);
+                cmd.Parameters.AddWithValue("@nombre", artista.Nombre);
+                cmd.Parameters.AddWithValue("@apellido", artista.Apellido);
+                cmd.Parameters.AddWithValue("@fechaNacimiento", artista.FechaNacimiento);
+                cmd.Parameters.AddWithValue("@nacionalidad", artista.Nacionalidad);
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+
+        // Obtener todos los artistas de la base de datos
+        public List<Artista> ObtenerArtistas()
+        {
+            List<Artista> artistas = new List<Artista>();
+            using (ConexionDB db = new ConexionDB())
+            {
+                string sql = "SELECT * FROM Artistas";
+                SqlCommand cmd = new SqlCommand(sql, db.Conexion);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        artistas.Add(new Artista
+                        {
+                            ArtistaId = Convert.ToInt32(reader["artista_id"]),
+                            Nombre = reader["nombre"].ToString(),
+                            Apellido = reader["apellido"].ToString(),
+                            FechaNacimiento = Convert.ToDateTime(reader["fecha_nacimiento"]),
+                            Nacionalidad = reader["nacionalidad"].ToString()
+                        });
+                    }
+                }
+            }
+            return artistas;
+        }
+
+        // Actualizar un artista existente en la base de datos
+        public bool ActualizarArtista(Artista artista)
+        {
+            using (ConexionDB db = new ConexionDB())
+            {
+                string sql = "UPDATE Artistas SET nombre=@nombre, apellido=@apellido, fecha_nacimiento=@fechaNacimiento, nacionalidad=@nacionalidad WHERE artista_id=@artistaId";
+                SqlCommand cmd = new SqlCommand(sql, db.Conexion);
+                cmd.Parameters.AddWithValue("@artistaId", artista.ArtistaId);
+                cmd.Parameters.AddWithValue("@nombre", artista.Nombre);
+                cmd.Parameters.AddWithValue("@apellido", artista.Apellido);
+                cmd.Parameters.AddWithValue("@fechaNacimiento", artista.FechaNacimiento);
+                cmd.Parameters.AddWithValue("@nacionalidad", artista.Nacionalidad);
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+
+        // Eliminar un artista de la base de datos
+        public bool EliminarArtista(int artistaId)
+        {
+            using (ConexionDB db = new ConexionDB())
+            {
+                string sql = "DELETE FROM Artistas WHERE artista_id=@artistaId";
+                SqlCommand cmd = new SqlCommand(sql, db.Conexion);
+                cmd.Parameters.AddWithValue("@artistaId", artistaId);
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+    }
+}
